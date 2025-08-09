0896d3396c89697dfa393fcba8bffb5189ddac0e
Arthur Pariente <arthur.pariente78960@gmail.com>
2025-08-09 11:59:07 +0200

    refactor: migrate SimBriefAirframes

---
+++ app/Filament/Resources/SimBriefAirframes/Pages/ManageSimBriefAirframes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\SimBriefAirframes\Pages;
+
+use App\Filament\Resources\SimBriefAirframes\SimBriefAirframeResource;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageSimBriefAirframes extends ManageRecords
+{
+    protected static string $resource = SimBriefAirframeResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
+}
